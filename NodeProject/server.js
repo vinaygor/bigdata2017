f9f9c04aa5739c9b4533a446d6503060c2037368
akshay <akshaysjk@gmail.com>
2017-12-09 20:37:53 -0500

    no message

--- NodeProject/server.js
+++ NodeProject/server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var https = require('https');
+var Client = require('node-rest-client').Client;
+
+var client = new Client();
 
 
 app.use(express.static(__dirname+"/public"));
@@ -16,6 +19,7 @@ app.get('/', function(request,response)
     });
 
 app.post('/submit',function(request,response){
+
   console.log("Submitting the request..");
   console.log("Request Body received:");
 	console.log(request.body);
@@ -28,16 +32,28 @@ app.post('/submit',function(request,response){
 	host = "ussouthcentral.services.azureml.net";
 	path = "/workspaces/87d2d38691514eac8f0baa4409e6bab3/services/64590943a7874f68a81d11e126df9c6f/execute?api-version=2.0&details=true";
 
-	var options = {
+	var args = {
 	 host: host,
 	 method: method,
 	 headers: headers,
 	 path: path,
 	 data: JSON.stringify(request.body)
  };
+ var url = "https://"+host+path;
+ client.post(url,args,function(data,response){
+	 console.log("XHR Response"+response);
+ });
+
+ client.registerMethod("postMethod", url, "POST");
+
+ client.methods.postMethod(args, function (data, response) {
+	 console.log("XHR Response"+response);
+ });
+
+
 console.log("===== OPTIONS DATA========");
-console.log(options);
- var req = https.request(options, function(res) {
+console.log(args);
+ var req = https.request(args, function(res) {
 	 console.log("Inside the response....");
 	res.setEncoding('utf-8');
 	console.log(res);
